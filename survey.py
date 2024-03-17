04ea055f4c9aadef3d705886319289b42f0550c1
airidasbrikas23 <airidas.brikas@gmail.com>
2024-03-17 17:08:47 +0100

    Update

--- survey.py
+++ survey.py
@@ -5,30 +5,46 @@ import pandas as pd
 class KanoSurvey:
 
     POSSIBLE_ANSWERS = ["I LIKE IT", "I EXPECT IT", "I AM NEUTRAL", "I CAN TOLERATE IT", "I DISLIKE IT"]
-    questions: List[str];
-    test_answers: Dict[str, List[str]];
-    _surv: pd.DataFrame;
+    questions: List[str]
+    test_answers: Dict[str, List[str]]
+    df: pd.DataFrame
 
-    def __init__(self, csv_file, default_instruction_text: str = None):
+
+    def __init__(self, csv_file):
+        # Parse Survey questions
+        df = pd.read_csv(csv_file)
+        df = df[df["type"] != 0]
+        self.questions = df["question"]
+
+        # Parse Test answers
         self.test_answers = {}
-        self._build(csv_file)
-        
-        if default_instruction_text is None:
-            self.default_instruction_text = f"Your answer should only contain the chosen option. Reply to the statement below with one of the five options: {', '.join(self.POSSIBLE_ANSWERS)}."
-        else:
-            self.default_instruction_text = default_instruction_text
+        try:
+            self.test_answers["airidas"] = list(df["airidas"])
+        except:
+            print("No airidas answers column found in the csv file")
+        try:
+            self.test_answers["elias"] = list(df["Elias"])
+        except:
+            print("No elias answers column found in the csv file")
 
-    def getDefaultPromptBlocks(self) -> List[str]:
-        sps = []
-        for q in self.surv["question"]:
-            sp = f"{self.default_instruction_text}\n\n{q}"
-            sps.append(sp)
-        return sps
+        self.df = df 
         
-    def _build(self, csv_file) -> List[str]:
+
+        
+class PersonalitySurvey:
+    
+    POSSIBLE_ANSWERS = ["DISAGREE", "SOMEWHAT DISAGREE", "NEUTRAL", "SOMEWHAT AGREE", "AGREE"]
+    questions: List[str]
+    test_answers: Dict[str, List[str]]
+    df: pd.DataFrame
+
+    def __init__(self, csv_file):
+        # Parse Survey questions
         surv = pd.read_csv(csv_file)
-        surv = surv[surv["type"] != 0]
         self.questions = surv["question"]
+
+        # Parse Test answers
+        self.test_answers = {}
         try:
             self.test_answers["airidas"] = list(surv["airidas"])
         except:
@@ -38,33 +54,33 @@ class KanoSurvey:
         except:
             print("No elias answers column found in the csv file")
 
-        self._surv = surv
-        
+        self.df = surv 
 
+class DictatorGameSurvey:
+    
+    POSSIBLE_ANSWERS = ["LEFT", "RIGHT"]
+    questions: List[str]
+    test_answers: Dict[str, List[str]]
+    df: pd.DataFrame
 
-def buildPersonalityTestPrompts(csv_file) -> List[str]:
-    surv = pd.read_csv(csv_file)
+    def __init__(self, csv_file):
+        # Parse Survey questions
+        surv = pd.read_csv(csv_file)
+        self.questions = surv["question"]
 
-    CONSTANT_TEXT = "Your answer should only contain the chosen option. Reply to the statement below with one of the five options: DISAGREE, SOMEWHAT DISAGREE, NEUTRAL, SOMEHWHAT AGREE, AGREE."
+        # Parse Test answers
+        self.test_answers = {}
+        try:
+            self.test_answers["airidas"] = list(surv["airidas"])
+        except:
+            print("No airidas answers column found in the csv file")
+        try:
+            self.test_answers["elias"] = list(surv["Elias"])
+        except:
+            print("No elias answers column found in the csv file")
 
-    sps = []
-    for q in surv["question"]:
-        sp = f"{CONSTANT_TEXT}\n\n{q}"
-        sps.append(sp)
+        self.df = surv 
 
-    return sps
-
-def buildDictatorGamePrompts(csv_file) -> List[str]:
-    surv = pd.read_csv(csv_file)
-
-    CONSTANT_TEXT = "You are given two options, LEFT or RIGHT. Select one of the options. Your answer should contain only the label of the option."
-
-    sps = []
-    for q in surv["question"]:
-        sp = f"{CONSTANT_TEXT}\n\n{q}"
-        sps.append(sp)
-
-    return sps
 
 def buildFairnessPrompts(csv_file) -> List[str]:
     surv = pd.read_csv(csv_file)
