daddfc6dcb2c00ad274e5c0dd4da05a6b40e3203
airidasbrikas23 <80290187+Brikas@users.noreply.github.com>
2024-03-28 21:19:23 +0100

    Update

--- shared_utils.py
+++ shared_utils.py
@@ -10,20 +10,6 @@ def count_tokens(string: str) -> int:
     return num_tokens
 
 
-def gpt(messages: list[Dict], model: str = "gpt-3.5-turbo", kwargs: dict = {}) -> str:
-    client = OpenAI(
-        # This is the default and can be omitted
-        api_key=os.environ.get("OPENAI_API_KEY"),
-    )
-
-    r = client.chat.completions.create(
-        messages=messages,
-        **kwargs,
-        model=model,
-    )
-    return r.choices[0].message.content
-
-
 BLACKLIST_CHAT_REGEX_FILTERS = [
     {
         "id": "link-filter",
@@ -33,4 +19,8 @@ BLACKLIST_CHAT_REGEX_FILTERS = [
         "id": "react-filter",
         "pattern": r"Reacted\s.*\sto\syour\smessage"
     }
+]
+
+BLACKLIST_ANSWER_SUBSTRINGS = [
+    "\."
 ]
