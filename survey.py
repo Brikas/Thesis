86a2cf4771e3f0ca9ed742d37e2721c1b67ceec8
airidasbrikas23 <80290187+Brikas@users.noreply.github.com>
2024-04-18 15:46:24 +0200

    QoL update: when making survey, no need to select CSV file. Default option added.

--- survey.py
+++ survey.py
@@ -4,13 +4,19 @@ import pandas as pd
 
 class KanoSurvey:
 
+    # Static Variables
     POSSIBLE_ANSWERS = ["I LIKE IT", "I EXPECT IT", "I AM NEUTRAL", "I CAN TOLERATE IT", "I DISLIKE IT"]
+    _defaultCSV = "surveys/survey_kano-model.csv"
+
+    # Instance Variables
     questions: List[str]
     test_answers: Dict[str, List[str]]
     df: pd.DataFrame
 
+    def __init__(self, csv_file = _defaultCSV):
+        if csv_file == KanoSurvey._defaultCSV:
+            print(f"Using default Kano Survey CSV file: {KanoSurvey._defaultCSV}")
 
-    def __init__(self, csv_file):
         # Parse Survey questions
         df = pd.read_csv(csv_file)
         df = df[df["type"] != 0]
@@ -32,13 +38,19 @@ class KanoSurvey:
 
         
 class PersonalitySurvey:
-    
+    # Static Variables
     POSSIBLE_ANSWERS = ["DISAGREE", "SOMEWHAT DISAGREE", "NEUTRAL", "SOMEWHAT AGREE", "AGREE"]
+    _defaultCSV = "surveys/survey_personality-test.csv"
+
+    # Instance Variables
     questions: List[str]
     test_answers: Dict[str, List[str]]
     df: pd.DataFrame
 
-    def __init__(self, csv_file):
+    def __init__(self, csv_file=_defaultCSV):
+        if csv_file == PersonalitySurvey._defaultCSV:
+            print(f"Using default Personality Survey CSV file: {PersonalitySurvey._defaultCSV}")
+
         # Parse Survey questions
         surv = pd.read_csv(csv_file)
         self.questions = surv["question"]
@@ -58,12 +70,19 @@ class PersonalitySurvey:
 
 class DictatorGameSurvey:
     
+    # Static Variables
     POSSIBLE_ANSWERS = ["LEFT", "RIGHT"]
+    _defaultCSV = "surveys/survey_dictator-game.csv"
+
+    # Instance Variables
     questions: List[str]
     test_answers: Dict[str, List[str]]
     df: pd.DataFrame
 
-    def __init__(self, csv_file):
+    def __init__(self, csv_file = _defaultCSV):
+        if csv_file == DictatorGameSurvey._defaultCSV:
+            print(f"Using default Dictator Game Survey CSV file: {DictatorGameSurvey._defaultCSV}")
+
         # Parse Survey questions
         surv = pd.read_csv(csv_file)
         self.questions = surv["question"]
@@ -82,12 +101,19 @@ class DictatorGameSurvey:
         self.df = surv 
 
 class FairnessSurvey:
+        # Static Variables
         POSSIBLE_ANSWERS = ["COMPLETELY FAIR", "ACCEPTABLE", "UNFAIR", "VERY UNFAIR"]
+        _defaultCSV = "surveys/survey_fairness.csv"
+
+        # Instance Variables
         questions: List[str]
         test_answers: Dict[str, List[str]]
         df: pd.DataFrame
     
-        def __init__(self, csv_file):
+        def __init__(self, csv_file=_defaultCSV):
+            if csv_file == FairnessSurvey._defaultCSV:
+                print(f"Using default Fairness Survey CSV file: {FairnessSurvey._defaultCSV}")
+
             # Parse Survey questions
             surv = pd.read_csv(csv_file)
             surv = surv[surv["framing"] == "Change"]
